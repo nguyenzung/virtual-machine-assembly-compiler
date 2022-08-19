8160fdb7bfaa966432444250eec0e5c15dc5bd11
nguyenzung <nguyenvietdungcs52@gmail.com>
2022-08-19 11:49:57 +0700

    compile for folder

--- translator.py
+++ translator.py
@@ -1,4 +1,5 @@
-import sys
+from genericpath import isdir, isfile
+import os, sys
 
 from vmparser import Parser
 from vmcodewriter import CodeWriter
@@ -8,7 +9,9 @@ def create_asm_file_name(file_name):
     return "{}.asm".format(parts[0])
 
 def parse_file_path(file_path):
+    file_path = file_path.rstrip("/")
     parts = file_path.split("/")
+    print(parts)
     file_name = parts[len(parts) - 1]
     path = "/".join(parts[0:len(parts) - 1])
     if path == "":
@@ -28,14 +31,51 @@ def translate(path, input_file_name, output_file_name):
     code_writer.finish()
     return
 
+
+def link(rootFile, files):
+    print("[LINKING]", rootFile, files)
+    open(rootFile, 'w').close()
+    rootFileData = open(rootFile, "a")
+    for file in files :
+        fin = open(file, "r")
+        data = fin.read()
+        rootFileData.write(data)
+        rootFileData.write("\n")
+        fin.close()
+    rootFileData.close()
+    return
+
+def translateFolder(path, input_folder_name):
+    print("[Translate folder] {0}:{1}".format(path, input_folder_name))
+    if path[len(path)-1:] != "/":
+                path = "{}/".format(path)
+    compiledFiles = ["bootstrap.asm.template", "{}Sys.asm".format(path)]
+    files = os.listdir(path)
+    for file in files:
+        if len(file) > 3 and file[len(file)-3:] == ".vm":            
+            outFile = translateFile(path, file)
+            if outFile != "Sys.asm":
+                compiledFiles.append(path + outFile)
+    link("{}{}.asm".format(path, input_folder_name), compiledFiles)
+    return
+
+def translateFile(path, input_file_name):
+    print("[Translate file] {}".format(input_file_name))
+    output_file_name = create_asm_file_name(input_file_name)
+    print(input_file_name, output_file_name)
+    translate(path, input_file_name, output_file_name)
+    return output_file_name
+
 def main():
     print("VM Translator")
     if len(sys.argv) != 2:
-        print("Usage: python3 translator.py <file_name.vm>")
+        print("Usage: python3 translator.py <file_name.vm>/<folder_name>")
         return
     path, input_file_name = parse_file_path(sys.argv[1])
-    output_file_name = create_asm_file_name(input_file_name)
-    translate(path, input_file_name, output_file_name)
+    if os.path.isdir(sys.argv[1]):
+        translateFolder(sys.argv[1], input_file_name)
+    elif os.path.isfile(sys.argv[1]):
+        translateFile(path, input_file_name)
     return
 
 if __name__ == "__main__":
